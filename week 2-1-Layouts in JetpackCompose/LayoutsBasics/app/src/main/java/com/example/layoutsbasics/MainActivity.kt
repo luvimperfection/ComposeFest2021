e9b887e4a85d516cf2a3df2151d33d2671509638
JungIn Choi <luvimperfection@gmail.com>
2021-11-08 06:20:16 +0900

    [Layouts in JetpackCompose] ~ 6.Working with lists

--- week 2-1-Layouts in JetpackCompose/LayoutsBasics/app/src/main/java/com/example/layoutsbasics/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/LayoutsBasics/app/src/main/java/com/example/layoutsbasics/MainActivity.kt
@@ -3,12 +3,10 @@ package com.example.layoutsbasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -16,20 +14,25 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.example.layoutsbasics.ui.theme.LayoutsBasicsTheme
 import com.example.layoutsbasics.ui.theme.LayoutsCodelabTheme
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PhotographerCardPreview()
+//            PhotographerCardPreview()
+            LayoutPracticePreview()
         }
     }
 }
@@ -50,7 +53,9 @@ fun LayoutsCodelab() {
             )
         }
     ){ innerPadding ->
-       BodyContent(Modifier.padding(innerPadding).padding(8.dp))
+       BodyContent(Modifier
+           .padding(innerPadding)
+           .padding(8.dp))
     }
 }
 
@@ -59,6 +64,51 @@ fun BodyContent(modifier: Modifier = Modifier) {
     Column(modifier = modifier) {
         Text(text = "Hi there")
         Text(text = "Thanks for going through the Layouts codelab")
+    }
+}
+
+@Composable
+fun SimpleList() {
+    val scrollState = rememberLazyListState()
+    val listSize = 100
+    val coroutineScope = rememberCoroutineScope()
+
+    Column{
+        Row{
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(0)
+                }
+            }){
+                Text("Scroll to the top")
+            }
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(listSize-1)
+                }
+            }){
+                Text("Scroll to the end")
+            }
+        }
+        LazyColumn(state = scrollState) {
+            items(100){
+                ImageListItem(it)
+            }
+        }
+    }
+
+}
+
+@Composable
+fun ImageListItem(index: Int){
+    Row(verticalAlignment = Alignment.CenterVertically){
+        Image(
+            painter = rememberImagePainter(data = "https://developer.android.com/images/brand/Android_Robot.png"),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Text("Item $index", style = MaterialTheme.typography.subtitle1)
     }
 }
 
@@ -95,9 +145,10 @@ fun PhotographerCard(modifier: Modifier = Modifier){
 
 @Preview
 @Composable
-fun PhotographerCardPreviewBasicsTheme() {
+fun LayoutPracticePreview() {
     LayoutsBasicsTheme {
-        LayoutsCodelab()
+        SimpleList()
+//        LayoutsCodelab()
     }
 }
 
